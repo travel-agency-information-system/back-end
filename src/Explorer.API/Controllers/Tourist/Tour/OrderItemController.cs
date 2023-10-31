515abc5e3462b770fab7d5cd5a4a33204b26f8e0
petrovicmilica <116906072+petrovicmilica@users.noreply.github.com>
2023-10-31 16:26:27 +0100

    feat: Finish back-end

---
+++ src/Explorer.API/Controllers/Tourist/Tour/OrderItemController.cs
@@ -0,0 +1,48 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Administration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Tourist.Tour
+{
+	[Authorize(Policy = "touristPolicy")]
+	[Route("api/tourist/order-item")]
+	public class OrderItemController : BaseApiController
+	{
+		private readonly IOrderItemService _orderItemService;
+
+		public OrderItemController(IOrderItemService orderItemService)
+		{
+			_orderItemService = orderItemService;
+		}
+
+		[HttpGet]
+		public ActionResult<PagedResult<OrderItemDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+		{
+			var result = _orderItemService.GetPaged(page, pageSize);
+			return CreateResponse(result);
+		}
+
+		[HttpPost]
+		public ActionResult<OrderItemDto> Create([FromBody] OrderItemDto orderItem)
+		{
+			var result = _orderItemService.Create(orderItem);
+			return CreateResponse(result);
+		}
+
+		[HttpPut("{id:int}")]
+		public ActionResult<OrderItemDto> Update([FromBody] OrderItemDto orderItem)
+		{
+			var result = _orderItemService.Update(orderItem);
+			return CreateResponse(result);
+		}
+
+		[HttpDelete("{id:int}")]
+		public ActionResult Delete(int id)
+		{
+			var result = _orderItemService.Delete(id);
+			return CreateResponse(result);
+		}
+	}
+}
---
+++ src/Explorer.API/Controllers/Tourist/Tour/ShoppingCartController.cs
@@ -0,0 +1,49 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Administration;
+using Explorer.Tours.Core.Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Tourist.Tour
+{
+	[Authorize(Policy = "touristPolicy")]
+	[Route("api/tourist/shopping-cart")]
+	public class ShoppingCartController : BaseApiController
+	{
+		private readonly IShoppingCartService _shoppingCartService;
+
+		public ShoppingCartController(IShoppingCartService shoppingCartService)
+		{
+			_shoppingCartService = shoppingCartService;
+		}
+
+		[HttpGet]
+		public ActionResult<PagedResult<ShoppingCartDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+		{
+			var result = _shoppingCartService.GetPaged(page, pageSize);
+			return CreateResponse(result);
+		}
+
+		[HttpPost]
+		public ActionResult<ShoppingCartDto> Create([FromBody] ShoppingCartDto shoppingCart)
+		{
+			var result = _shoppingCartService.Create(shoppingCart);
+			return CreateResponse(result);
+		}
+
+		[HttpPut("{id:int}")]
+		public ActionResult<ShoppingCartDto> Update([FromBody] ShoppingCartDto shoppingCart)
+		{
+			var result = _shoppingCartService.Update(shoppingCart);
+			return CreateResponse(result);
+		}
+
+		[HttpDelete("{id:int}")]
+		public ActionResult Delete(int id)
+		{
+			var result = _shoppingCartService.Delete(id);
+			return CreateResponse(result);
+		}
+	}
+}
